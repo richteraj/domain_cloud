530d74ddb4f29f04dfb37e95394a3a632782753a
A. Johannes RICHTER <albrechtjohannes.richter@gmail.com>
2017-09-16 21:01:58 +0200

    tests: document shminitests.sh
    * doc/Doxyfile-dev.in (EXTENSION_MAPPING, FILE_PATTERNS): Treat *.sh
    files as Python source to recognize at least some documentation with
    Doxygen.  Only for the debug release.
    * tests/shminitests.sh: Add Doxygen doc although *.sh files are not
    supported by Doxygen.

--- tests/shminitests.sh
+++ tests/shminitests.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+## \file
+#  Test cases with the shell.  Source this file to use the functions.
+#
+#  Sample usage:
+#   \li call \ref tests_start
+#   \li set the test name to \ref test_case and do some testing
+#   \li store the result (interpretation as exit status) at \ref test_exit
+#   \li call \ref evaluate_test
+#   \li repeat for other test cases
+#   \li call \ref tests_end
+
+# Some terminal colors by name.
 none="\033[0m"
 blue="\033[0;34m"
 green="\033[0;32m"
@@ -7,9 +19,20 @@ red="\033[0;31m"
 brown="\033[0;33m"
 cyan="\033[0;35m"
 
-tests_failed=0
+## Total number of run test cases.
 tests_count=0
+## Total number of failed tests.
+tests_failed=0
+## Name of the current test case
+test_case=""
+## Result (exit status) of the current test case.
+test_exit=0
 
+## Print whether current \ref test_case was successful which is determined by
+#  \ref test_exit.  If the test was unsuccessful (<tt>test_exit != 0</tt>)
+#  print an optional message.
+#
+#  \param $1 Optional error message.
 evaluate_test ()
 {
     tests_count=`expr $tests_count + 1`
@@ -25,11 +48,14 @@ evaluate_test ()
     fi
 }
 
+## Call before any test case.
 tests_start ()
 {
     echo "$blue------ RUNNING: `basename $0`$none" >&2
 }
 
+## Call after all test cases.  Exits with 1 if \ref tests_failed is not 0 or
+#  exits with 0 else.
 tests_end ()
 {
     echo "$blue------ FINISHED: `basename $0`$none" >&2
@@ -42,4 +68,3 @@ tests_end ()
         exit 1
     fi
 }
-
