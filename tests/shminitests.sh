5c4209c52f9eeb2863faa440a80ead2debf4a4eb
A. Johannes RICHTER <albrechtjohannes.richter@gmail.com>
2017-10-06 23:57:27 +0200

    tests: update shminitests.sh
    * tests/shminitests.sh: See https://github.com/richteraj/cminitests.
    * tests/test_cli_options.sh: Remove unneeded test_exit=$? statements.

--- tests/shminitests.sh
+++ tests/shminitests.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 ## \file
-# Test cases with the shell.  Source this file to use the functions.
+#  Test cases with the shell.  Source this file to use the functions.
 #
-# Sample usage:
+#  Sample usage:
 #   \li call \ref tests_start
 #   \li set the test name to \ref test_case and do some testing
 #   \li store the result (interpretation as exit status) at \ref test_exit
@@ -23,19 +23,23 @@ cyan="\033[0;35m"
 tests_count=0
 ## Total number of failed tests.
 tests_failed=0
-## Name of the current test case.
+## Name of the current test case
 test_case=""
 ## Result (exit status) of the current test case.
-test_exit=0
+test_exit=
 
 ## Print whether current \ref test_case was successful which is determined by
-# \ref test_exit.  If the test was unsuccessful (`test_exit != 0`) print
-# an optional message.
+# \ref test_exit.  If the test was unsuccessful (`test_exit != 0`) print an
+# optional message.
+# If test_exit is not set (or null string) use $? for this.  The test_exit
+# variable is unset after this call.
 #
 # \param $1 Optional error message.
 evaluate_test ()
 {
+    test_exit=${test_exit:-$?}
     tests_count=`expr $tests_count + 1`
+
     echo -n "$test_case: " >&2
     if [ "$test_exit" -eq 0 ]; then
         echo "${green}passed$none." >&2
@@ -46,19 +50,42 @@ evaluate_test ()
             echo " >>> $brown$1$none" >&2
         fi
     fi
+    test_exit=
 }
 
-## Call before any test case.
+## Call \ref evaluate_test with \a $1 and automatically set \ref test_case and
+# \ref test_exit.
+#
+# \param $1 The function which represents a test case.  Should "return" 0 on
+# success, i.e. the return state of the last command in the function is
+# evaluated for \ref test_exit.
+# \param $2 An optional name for \ref test_case.  Defaults to \a $1 if not set
+# (or null string).
+# \param $3 An optional error message that is passed on to \ref evaluate_test.
+execute_test_case ()
+{
+    test_function=$1
+    test_case=${2:-$test_function}
+    "$test_function"
+    evaluate_test "$3"
+}
+
+## Call before any test case.  Will set the initial values of the global
+# variables.
 tests_start ()
 {
-    echo "$blue------ RUNNING: `basename $0`$none" >&2
+    tests_count=0
+    tests_failed=0
+    test_case=""
+    test_exit=
+    echo "${blue}------ RUNNING: `basename $0`$none" >&2
 }
 
 ## Call after all test cases.  Exits with 1 if \ref tests_failed is not 0 or
 # exits with 0 else.
 tests_end ()
 {
-    echo "$blue------ FINISHED: `basename $0`$none" >&2
+    echo "${blue}------ FINISHED: `basename $0`$none" >&2
     if [ "$tests_failed" -eq 0 ]; then
         echo "${green}------ ALL $tests_count tests passed.$none" >&2
         exit 0
@@ -68,3 +95,19 @@ tests_end ()
         exit 1
     fi
 }
+
+# Copyright 2017 A. Johannes RICHTER <albrechtjohannes.richter@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
--- tests/test_cli_options.sh
+++ tests/test_cli_options.sh
@@ -9,12 +9,10 @@ tests_start
 
 test_case="Program with no arguments has nonzero exit"
 ! "$prog" >/dev/null 2>&1
-test_exit=$?
 evaluate_test
 
 test_case="Program with no arguments prints an error message"
 "$prog" 2>&1 | grep -q  "No input files!"
-test_exit=$?
 evaluate_test 'grep -q  "No input files!"'
 
 input_file="`mktemp`"
@@ -23,7 +21,6 @@ output_file="`mktemp`"
 test_case="Program doesn't fails if there are no words remaining"
 echo "// Only comments...\n\"and strings...\"" >"$input_file"
 "$prog" -S "$input_file" >/dev/null 2>&1
-test_exit=$?
 evaluate_test
 
 test_case="Program skips non-readable input files"
@@ -31,24 +28,20 @@ echo "some words" >"$input_file"
 bad_input_file="Not a file 1"
 "$prog" -S "$bad_input_file" "$input_file" 2>&1 | \
     grep -q "Can't open '$bad_input_file'!: No such file or directory"
-test_exit=$?
 evaluate_test
 
 test_case="Program skips non-readable input files and doesn't fail"
 echo "some words" >"$input_file"
 bad_input_file="Not a file 1"
 "$prog" -S "$bad_input_file" "$input_file" >/dev/null 2>&1
-test_exit=$?
 evaluate_test
 
 test_case="Program fails if it can't write to output file"
 ! "$prog" -S -o "/no_root_access" "$input_file" 2>/dev/null
-test_exit=$?
 evaluate_test
 
 test_case="Program recognizes '-' as standard in/output"
 echo "/* skip */some_words" | "$prog" -S -o - - | grep -q "some_words"
-test_exit=$?
 evaluate_test
 
 test_case="Program writes output to given file"
